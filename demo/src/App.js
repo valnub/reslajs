049c8507b1f47957b1f6812144dfd3873d5f2377
Timo <mail@timo-ernst.net>
2022-02-12 20:51:18 +0100

    Fixed buttongrid-item component to work with outer state

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,9 +2,24 @@ import './App.css';
 import React, { useState } from 'react';
 import { Button, Toggle, ButtonGrid, ButtonGridItem, Slider } from 'reslajs';
 
+function copy(item) {
+  return JSON.parse(JSON.stringify(item));
+}
+
 function App() {
   const [checked, setChecked] = useState(false);
   const [sliderValue, setSliderValue] = useState(0);
+  const [carFunctions, setCarFunctions] = useState([
+    { id: 'func0', label: 'Lights', active: false },
+    { id: 'func1', label: 'Heater', active: false },
+    { id: 'func2', label: 'Radio', active: true },
+    { id: 'func3', label: 'Spotify', active: false },
+    { id: 'func4', label: 'Massage', active: false },
+    { id: 'func5', label: 'Camera', active: true },
+    { id: 'func6', label: 'Sentry mode', active: false },
+    { id: 'func7', label: 'Fan', active: false },
+  ]);
+
   return (
     <>
       <Button
@@ -49,14 +64,23 @@ function App() {
           marginRight: 'auto',
         }}
       >
-        <ButtonGridItem>Lights</ButtonGridItem>
-        <ButtonGridItem>Heater</ButtonGridItem>
-        <ButtonGridItem>Radio</ButtonGridItem>
-        <ButtonGridItem>Spotify</ButtonGridItem>
-        <ButtonGridItem>Massage</ButtonGridItem>
-        <ButtonGridItem>Camera</ButtonGridItem>
-        <ButtonGridItem>Sentry mode</ButtonGridItem>
-        <ButtonGridItem>Fan</ButtonGridItem>
+        {carFunctions.map((carFunction) => {
+          return (
+            <ButtonGridItem
+              id={carFunction.id}
+              key={Math.random()}
+              active={carFunction.active}
+              onClick={(event, id, newState) => {
+                const carFuncsCopy = copy(carFunctions);
+                const foundCarFunc = carFuncsCopy.find((el) => el.id === id);
+                foundCarFunc.active = newState;
+                setCarFunctions(carFuncsCopy);
+              }}
+            >
+              {carFunction.label}
+            </ButtonGridItem>
+          );
+        })}
       </ButtonGrid>
     </>
   );
--- src/buttongrid-item/buttongrid-item.jsx
+++ src/buttongrid-item/buttongrid-item.jsx
@@ -2,16 +2,14 @@ import React, { useState } from 'react';
 import './buttongrid-item.css';
 
 export default function ButtonGridItem(props) {
-  const { id, className, style, onClick, children } = props;
-  const [active, setActive] = useState(false);
+  const { id, className, style, onClick, active, children } = props;
 
   return (
     <div
       id={id}
       style={style}
       onClick={(e) => {
-        setActive(!active);
-        if (onClick) onClick(e);
+        if (onClick) onClick(e, id, !active);
       }}
       role="switch"
       className={`reslajs-buttongrid-item ${
